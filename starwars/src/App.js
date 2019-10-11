35215dce342456cff268b63d1ed6883672a8e95d
Zack Bumpous <upwork.ztb@gmail.com>
2019-10-11 11:12:17 -0500

    renamed a file for my own memory purposes and an working on my function to get the API data

--- starwars/src/App.js
+++ starwars/src/App.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import './App.css';
-import ProfileList from './components/ProfileList.js'
+import JediList from './components/JediList.js'
 
 const App = () => {
   // Try to think through what state you'll need for this app before starting. Then build out
@@ -13,7 +13,7 @@ const App = () => {
   return (
     <div className="App">
       <h1 className="Header">React Wars</h1>
-    <ProfileList />
+    <JediList />
     </div>
   );
 }
---
+++ starwars/src/components/JediList.js
@@ -0,0 +1,14 @@
+//start off w imports
+import React, {useState, useEffect} from 'react';
+import axios from 'axios';
+
+//function to get my api and catch errors, gonna export default so i dont forget
+
+export default function starWarsAPIGet(){
+    const [dataAPI, setDataAPI] = useState([]);
+
+    useEffect(() => {
+        
+    },[])
+}
+
